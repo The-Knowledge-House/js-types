7fb55ad885580b34b7b528bbc73b4fdf873aaebb
NashidC <nash.chowdhury@gmail.com>
2020-09-21 17:27:37 -0400

    updated homework

--- script.js
+++ script.js
@@ -9,20 +9,20 @@
  * Assign q1 a value of type boolean
  */
 
-const q1 = null;
+const q1 = true;
 
 /**
  * QUESTION 2
  * Assign q2 a value of type number
  */
-const q2 = null;
+const q2 = 2;
 
 
 /**
  * QUESTION 3
  * Assign q3 a value of type string
  */
-const q3 = null;
+const q3 = "hello";
 
 
 /**
@@ -34,7 +34,7 @@ const q3 = null;
 
 
 
-const q4 = null;
+const q4 = "hello" * 5;
 
 
 /**
@@ -42,14 +42,14 @@ const q4 = null;
  * Assign q5 a value of type boolean. Produce the value by performing
  * a comparison operation on two numbers.
  */
-const q5 = null; 
+const q5 = (2 != 3); 
 
 
 /**
  * QUESTION 6
  * Assign q6 a falsey value that is not the boolean false
  */
-const q6 = null;   
+const q6 = !!0;   
 
 /**
  * For use in the next few questions, 
@@ -65,20 +65,21 @@ But, in a larger sense, we can not dedicate—we can not consecrate—we can not
  * 
  * Assign a version of the Gettysburg address converted to all capital letters.
  */
-const q7 = null;
+const q7 = address.toUpperCase();
 
 /**
  * QUESTION 8
  * Assign the number of characters in the Gettysburg Address
  */
-const q8 = null;
+const q8 = address.length;
 
 /**
  * QUESTION 8
  * Assign the result of splitting the Gettysburg Address on spaces
  * (use the same variable `address` to get started)
  */
-const q9 = null; 
+const q9 = address.split(" "); 
+
 
 
 /**
@@ -86,7 +87,8 @@ const q9 = null;
  * Assign q9 the number of words in the Gettysburg Address
  * (use your answer from above)
  */
-const q10 = null; 
+const q10 = address.split(" ").length; 
+
 
 
 /**
@@ -95,7 +97,15 @@ const q10 = null;
  * Loop over that array and build a new array with the periods (.)
  * removed from each word. Assign q11 the result.
  */
-const q11 = null;
+
+
+const updatedAddress = address.split(" ");
+
+for (let i = 0; i < updatedAddress.length; i++) {
+  updatedAddress[i] = updatedAddress[i].replace(".", "");
+}
+
+const q11 = updatedAddress;
 
 
 /**
